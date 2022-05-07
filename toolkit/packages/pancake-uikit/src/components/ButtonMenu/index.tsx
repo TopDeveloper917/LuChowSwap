c1efbdbf420674fcf80643d48011c56cefac4ed9
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-05-08 01:04:03 +0800

    Update index.tsx

--- toolkit/packages/pancake-uikit/src/components/ButtonMenu/index.tsx
+++ toolkit/packages/pancake-uikit/src/components/ButtonMenu/index.tsx
@@ -1,3 +1,4 @@
 export { default as ButtonMenu } from "./ButtonMenu";
 export { default as ButtonMenuItem } from "./ButtonMenuItem";
+export { default as LotteryMenuItem } from "./LotteryMenuItem";
 export type { ButtonMenuProps, ButtonMenuItemProps } from "./types";
