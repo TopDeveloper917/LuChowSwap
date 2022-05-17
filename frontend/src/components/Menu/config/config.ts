98bcf0bcd9880533943de5b8175502fc60aee5c4
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-05-18 01:04:47 +0800

    Update config.ts

--- frontend/src/components/Menu/config/config.ts
+++ frontend/src/components/Menu/config/config.ts
@@ -69,46 +69,46 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   //     },
   //   ],
   // },
-  // {
-  //   label: '',
-  //   href: '/info',
-  //   icon: 'More',
-  //   hideSubNav: true,
-  //   items: [
-  //     {
-  //       label: t('Info'),
-  //       href: '/info',
-  //     },
-  //     {
-  //       label: t('IFO'),
-  //       href: '/ifo',
-  //     },
-  //     {
-  //       label: t('Voting'),
-  //       href: '/voting',
-  //     },
-  //     {
-  //       type: DropdownMenuItemType.DIVIDER,
-  //     },
-  //     {
-  //       label: t('Leaderboard'),
-  //       href: '/teams',
-  //     },
-  //     {
-  //       type: DropdownMenuItemType.DIVIDER,
-  //     },
-  //     {
-  //       label: t('Blog'),
-  //       href: 'https://medium.com/pancakeswap',
-  //       type: DropdownMenuItemType.EXTERNAL_LINK,
-  //     },
-  //     {
-  //       label: t('Docs'),
-  //       href: 'https://docs.pancakeswap.finance',
-  //       type: DropdownMenuItemType.EXTERNAL_LINK,
-  //     },
-  //   ],
-  // },
+  {
+    label: '',
+    href: '/info',
+    icon: 'More',
+    hideSubNav: true,
+    items: [
+      {
+        label: t('Info'),
+        href: '/info',
+      },
+      // {
+      //   label: t('IFO'),
+      //   href: '/ifo',
+      // },
+      // {
+      //   label: t('Voting'),
+      //   href: '/voting',
+      // },
+      // {
+      //   type: DropdownMenuItemType.DIVIDER,
+      // },
+      // {
+      //   label: t('Leaderboard'),
+      //   href: '/teams',
+      // },
+      // {
+      //   type: DropdownMenuItemType.DIVIDER,
+      // },
+      // {
+      //   label: t('Blog'),
+      //   href: 'https://medium.com/pancakeswap',
+      //   type: DropdownMenuItemType.EXTERNAL_LINK,
+      // },
+      // {
+      //   label: t('Docs'),
+      //   href: 'https://docs.pancakeswap.finance',
+      //   type: DropdownMenuItemType.EXTERNAL_LINK,
+      // },
+    ],
+  },
 ]
 
 export default config
