70d7a7d8c7c803a94e5ecb77b7dc2585dffd0d33
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-05-20 01:20:27 +0800

    Create client.ts

---
+++ luchow-info-api/utils/apollo/client.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core";
+import fetch from "cross-fetch";
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    fetch,
+    uri: "https://api.thegraph.com/subgraphs/name/chenxiwang177/luchow-info",
+  }),
+  cache: new InMemoryCache(),
+});
