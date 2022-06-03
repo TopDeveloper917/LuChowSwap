dee0f9295ace82de71ed7358e1acb1ae2f3a0290
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-03 23:10:32 +0800

    Update RoiCalculatorFooter.tsx

--- frontend/src/components/RoiCalculatorModal/RoiCalculatorFooter.tsx
+++ frontend/src/components/RoiCalculatorModal/RoiCalculatorFooter.tsx
@@ -58,11 +58,11 @@ const RoiCalculatorFooter: React.FC<RoiCalculatorFooterProps> = ({
     <>
       <Text>
         {t(
-          'The Multiplier represents the proportion of CAKE rewards each farm receives, as a proportion of the CAKE produced each block.',
+          'The Multiplier represents the proportion of MLOKY rewards each farm receives, as a proportion of the MLOKY produced each block.',
         )}
       </Text>
       <Text my="24px">
-        {t('For example, if a 1x farm received 1 CAKE per block, a 40x farm would receive 40 CAKE per block.')}
+        {t('For example, if a 1x farm received 1 MLOKY per block, a 40x farm would receive 40 MLOKY per block.')}
       </Text>
       <Text>{t('This amount is already included in all APR calculations for the farm.')}</Text>
     </>,
@@ -91,7 +91,7 @@ const RoiCalculatorFooter: React.FC<RoiCalculatorFooterProps> = ({
               </>
             )}
             <Text color="textSubtle" small>
-              {isFarm ? t('Base APR (CAKE yield only)') : t('APR')}
+              {isFarm ? t('Base APR (MLOKY yield only)') : t('APR')}
             </Text>
             <Text small textAlign="right">
               {apr.toFixed(2)}%
