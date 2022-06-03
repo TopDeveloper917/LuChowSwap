edb61924b35575e4d2c4fc4004b8c3eddb6d0ad5
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-03 23:10:45 +0800

    Create customTokens.ts

---
+++ frontend/src/config/constants/customTokens.ts
@@ -0,0 +1,17 @@
+
+export const mainTokens = {
+    luchow: {
+        address: '0xe4e8e6878718bfe533702D4a6571Eb74D79b0915',
+        decimals: 18,
+        symbol: 'LUCHOW',
+        name: 'LuchowSwap Token',
+        logo: '/images/tokens/0xe4e8e6878718bfe533702D4a6571Eb74D79b0915.svg'
+    },
+    mloky: {
+        address: '0xF71E950758310faF9f7D51C4F4250C7546086C1f',
+        decimals: 9,
+        symbol: 'MLOKY',
+        name: 'MLOKY Token',
+        logo: '/images/tokens/0xF71E950758310faF9f7D51C4F4250C7546086C1f.svg'
+    }
+}
