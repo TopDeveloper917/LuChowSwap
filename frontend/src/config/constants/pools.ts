10d3985f85a46505bdf26984cb0d73fb32b7afa0
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-03 23:10:57 +0800

    Update pools.ts

--- frontend/src/config/constants/pools.ts
+++ frontend/src/config/constants/pools.ts
@@ -17,6 +17,48 @@ const pools: SerializedPoolConfig[] = [
     tokenPerBlock: '0.1',
     sortOrder: 1,
     isFinished: false,
+  },
+  {
+    sousId: 1,
+    stakingToken: serializedTokens.luchow,
+    earningToken: serializedTokens.mloky,
+    contractAddress: {
+      97: '',
+      56: '0x4319a5FEa245522D5016705f52910b4aB9F4D6A8',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.1',
+    sortOrder: 1,
+    isFinished: true,
+  },
+  {
+    sousId: 2,
+    stakingToken: serializedTokens.luchow,
+    earningToken: serializedTokens.mloky,
+    contractAddress: {
+      97: '',
+      56: '0xceF722E1277977e4a153Fd2f35030b84bf38Bb64',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.1',
+    sortOrder: 1,
+    isFinished: true,
+  },
+  {
+    sousId: 3,
+    stakingToken: serializedTokens.luchow,
+    earningToken: serializedTokens.mloky,
+    contractAddress: {
+      97: '',
+      56: '0x3631C2738945A544C42F6B06A353F0f35984377a',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.1',
+    sortOrder: 1,
+    isFinished: false,
   }
 ]
 
