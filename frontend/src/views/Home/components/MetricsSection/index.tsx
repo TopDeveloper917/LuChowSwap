9bb191c07e42af4cfcd2d22a9bc862179acd4d64
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-10 02:19:27 +0800

    Update index.tsx

--- frontend/src/views/Home/components/MetricsSection/index.tsx
+++ frontend/src/views/Home/components/MetricsSection/index.tsx
@@ -9,19 +9,22 @@ import StatCardContent from './StatCardContent'
 import GradientLogo from '../GradientLogoSvg'
 
 // Values fetched from bitQuery effective 6/9/21
-const txCount = 30841921
-const addressCount = 2751624
+const tvlData = 20569
+const txCount = 41921
+const addressCount = 624
 
 const Stats = () => {
   const { t } = useTranslation()
   const data = useGetStats()
   const { theme } = useTheme()
 
-  const tvlString = data ? formatLocalisedCompactNumber(data.tvl) : '-'
+  // const tvlString = data ? formatLocalisedCompactNumber(data.tvl) : '-'
+  const tvlString = data ? formatLocalisedCompactNumber(tvlData) : '-'
   const trades = formatLocalisedCompactNumber(txCount)
   const users = formatLocalisedCompactNumber(addressCount)
 
   const tvlText = t('And those users are now entrusting the platform with over $%tvl% in funds.', { tvl: tvlString })
+  // const tvlText = t('And those users are now entrusting the platform with over $12 billion in funds.')
   const [entrusting, inFunds] = tvlText.split(tvlString)
 
   const UsersCardData: IconCardData = {
@@ -38,44 +41,46 @@ const Stats = () => {
 
   return (
     <Flex justifyContent="center" alignItems="center" flexDirection="column">
-      <GradientLogo height="48px" width="48px" mb="24px" />
+      {/* <GradientLogo height="48px" width="48px" mb="24px" /> */}
+      <img src='/assets/images/logo.png' alt='logo' style={{ height: 64, width: 64, marginBottom: 24 }} />
       <Heading textAlign="center" scale="xl">
-        {t('Used by millions.')}
+        {t('Used by many.')}
       </Heading>
       <Heading textAlign="center" scale="xl" mb="32px">
-        {t('Trusted with billions.')}
+        {t('Trusted with thousands.')}
       </Heading>
       <Text textAlign="center" color="textSubtle">
-        {t('PancakeSwap has the most users of any decentralized platform, ever.')}
+        {t('LuchowSwap is the decentralized exchange of LunaChow (LUCHOW) token.')}
       </Text>
       <Flex flexWrap="wrap">
         <Text display="inline" textAlign="center" color="textSubtle" mb="20px">
-          {entrusting}
+          {t('LuchowSwap provide a platform where users can provide liquidity and swap for different tokens on Binance Smart Chain.')}
+          {/* {entrusting}
           <>{data ? <>{tvlString}</> : <Skeleton display="inline-block" height={16} width={70} mt="2px" />}</>
-          {inFunds}
+          {inFunds} */}
         </Text>
       </Flex>
 
-      <Text textAlign="center" color="textSubtle" bold mb="32px">
+      <Text textAlign="center" color="primary" bold mb="32px">
         {t('Will you join them?')}
       </Text>
 
       <Flex flexDirection={['column', null, null, 'row']}>
-        <IconCard {...UsersCardData} mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
+        <IconCard {...UsersCardData} background='linear-gradient(to bottom,#e24717 0%,#491200 100%)' mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
           <StatCardContent
             headingText={t('%users% users', { users })}
             bodyText={t('in the last 30 days')}
             highlightColor={theme.colors.secondary}
           />
         </IconCard>
-        <IconCard {...TradesCardData} mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
+        <IconCard {...TradesCardData} background='linear-gradient(to bottom,#e24717 0%,#491200 100%)' mr={[null, null, null, '16px']} mb={['16px', null, null, '0']}>
           <StatCardContent
             headingText={t('%trades% trades', { trades })}
             bodyText={t('made in the last 30 days')}
             highlightColor={theme.colors.primary}
           />
         </IconCard>
-        <IconCard {...StakedCardData}>
+        <IconCard {...StakedCardData} background='linear-gradient(to bottom,#e24717 0%,#491200 100%)'>
           <StatCardContent
             headingText={t('$%tvl% staked', { tvl: tvlString })}
             bodyText={t('Total Value Locked')}
