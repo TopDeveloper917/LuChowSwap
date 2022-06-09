ea0de203af00606482038a7d83e3dbd0770cf3c5
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-10 02:18:47 +0800

    Update HarvestAction.tsx

--- frontend/src/views/Farms/components/FarmCard/HarvestAction.tsx
+++ frontend/src/views/Farms/components/FarmCard/HarvestAction.tsx
@@ -25,7 +25,7 @@ const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, pid }) => {
   const { onReward } = useHarvestFarm(pid)
   const cakePrice = usePriceCakeBusd()
   const dispatch = useAppDispatch()
-  const rawEarningsBalance = account ? getBalanceAmount(earnings) : BIG_ZERO
+  const rawEarningsBalance = account ? getBalanceAmount(earnings, 9) : BIG_ZERO
   const displayBalance = rawEarningsBalance.toFixed(3, BigNumber.ROUND_DOWN)
   const earningsBusd = rawEarningsBalance ? rawEarningsBalance.multipliedBy(cakePrice).toNumber() : 0
 
