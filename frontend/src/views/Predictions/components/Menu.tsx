56945f69b53e36ec54bb70a960f17c08850ff43d
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-27 09:08:39 +0800

    Update Menu.tsx

--- frontend/src/views/Predictions/components/Menu.tsx
+++ frontend/src/views/Predictions/components/Menu.tsx
@@ -75,7 +75,7 @@ const Menu = () => {
             <Button
               variant="subtle"
               as="a"
-              href="https://docs.pancakeswap.finance/products/prediction"
+              href="https://docs.app.luchowswa.com/products/prediction"
               target="_blank"
               rel="noreferrer noopener"
               width="48px"
