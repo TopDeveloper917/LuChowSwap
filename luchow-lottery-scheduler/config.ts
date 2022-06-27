be7b63cb38e18f7155ffa97189db9c79bd645d29
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-28 01:44:32 +0800

    Create config.ts

---
+++ luchow-lottery-scheduler/config.ts
@@ -0,0 +1,42 @@
+export default {
+  Lottery: {
+    mainnet: "0xb4796a5aC0CD46e4cA28432c9147780A138f6726",
+    testnet: "",
+  },
+  Chainlink: {
+    Oracle: {
+      // Documentation: https://docs.chain.link/docs/binance-smart-chain-addresses/
+      mainnet: "0xB6064eD41d4f67e353768aA239cA86f4F73665a1",
+      testnet: "0x81faeDDfeBc2F8Ac524327d70Cf913001732224C",
+    },
+    VRF: {
+      // Documentation: https://docs.chain.link/docs/vrf-contracts/
+      KeyHash: {
+        mainnet: "0xc251acd21ec4fb7f31bb8868288bfdbaeb4fbfec2df3735ddbd4f7dc8d60103c",
+        testnet: "0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186",
+      },
+    },
+  },
+  Ticket: {
+    Price: {
+      mainnet: 1000000,
+      testnet: 1000000,
+    },
+    Precision: {
+      mainnet: 2,
+      testnet: 2,
+    },
+  },
+  Discount: {
+    mainnet: 2000,
+    testnet: 2000,
+  },
+  Rewards: {
+    mainnet: [250,375,625,1250,1875,5625],
+    testnet: [250,375,625,1250,1875,5625],
+  },
+  Treasury: {
+    mainnet: 2000,
+    testnet: 2000,
+  },
+};
