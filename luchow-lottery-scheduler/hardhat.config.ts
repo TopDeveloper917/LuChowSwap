35aa6e7f1b16bd138270e208e4852746ef53aef1
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-28 01:44:34 +0800

    Create hardhat.config.ts

---
+++ luchow-lottery-scheduler/hardhat.config.ts
@@ -0,0 +1,31 @@
+import type { HardhatUserConfig } from "hardhat/types";
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+task("accounts", "Prints the list of accounts", async (args, { ethers }) => {
+  const [operator] = await ethers.getSigners();
+
+  console.log(`Operator address: ${operator.address}`);
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  defaultNetwork: "testnet",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: [process.env.OPERATOR_PRIVATE_KEY!],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: [process.env.OPERATOR_PRIVATE_KEY!],
+    },
+  },
+};
+
+export default config;
