f7c0b6228e8305a6dbfbe64a979be1dbcbf4650d
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-06-27 09:08:28 +0800

    Update config.tsx

--- frontend/src/views/PancakeSquad/components/EventStepsSection/config.tsx
+++ frontend/src/views/PancakeSquad/components/EventStepsSection/config.tsx
@@ -57,7 +57,7 @@ const stepsConfigBuilder = ({ t, userInfos, eventInfos, userStatus, account, the
         <>
           <Button
             as="a"
-            href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+            href="https://app.luchowswa.com/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
             target="_blank"
             width="100%"
           >
