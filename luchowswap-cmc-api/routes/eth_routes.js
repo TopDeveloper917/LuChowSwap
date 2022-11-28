40ebce7b7910eca03441f49411d665019cb70639
topdeveloper917 <jamessmith0917topdev@gmail.com>
2022-11-29 02:28:57 +0800

    Create eth_routes.js

---
+++ luchowswap-cmc-api/routes/eth_routes.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let router = express.Router();
+
+let api_controller = require('../controllers/eth/ApiController');
+
+router.get('/summary', function (req, res, next) {
+    api_controller.summary(req, res, next);
+});
+router.get('/tokens', function (req, res, next) {
+    api_controller.tokens(req, res, next);
+});
+router.get('/tokens/:address', function (req, res, next) {
+    api_controller.tokensFromAddress(req, res, next);
+});
+router.get('/pairs', function (req, res, next) {
+    api_controller.pairs(req, res, next);
+});
+router.get('/pairs/:address', function (req, res, next) {
+    api_controller.pairsFromAddress(req, res, next);
+});
+
+module.exports = router;
